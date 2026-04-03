75019e7f9a31ee92cee2dba25b9a88f706245236
jhashank <jhashank@nuevesolutions.in>
2026-04-03 12:54:05 +0000

    tasks in variables and data types

---
+++ VariablesAndDataTypes/Tasks.js
@@ -0,0 +1,76 @@
+//name 
+const name1 = "John Doe 1";
+let name2 = "John Doe 2";
+var name = "John Doe 3";
+console.log(name1);
+console.log(name2);
+console.log(name3);
+console.log(is instanceof name1);
+console.log(is instanceof name2);
+console.log(is instanceof name3);
+
+//age
+const age1 = 30;
+let age2 = 25;
+var age3 = 40;
+console.log(age1);
+console.log(age2);
+console.log(age3);
+console.log(is instanceof age1);
+console.log(is instanceof age2);
+console.log(is instanceof age3);
+
+//isStudent
+const isStudent1 = true;
+let isStudent2 = false;
+var isStudent3 = true;
+console.log(isStudent1);
+console.log(isStudent2);
+console.log(isStudent3);
+console.log(is instanceof isStudent1);
+console.log(is instanceof isStudent2);
+console.log(is instanceof isStudent3);
+
+//favoriteColor
+const favoriteColor1 = "Blue";
+let favoriteColor2 = "Red";
+var favoriteColor3 = "Green";
+console.log(favoriteColor1);
+console.log(favoriteColor2);
+console.log(favoriteColor3);
+console.log(is instanceof favoriteColor1);
+console.log(is instanceof favoriteColor2);
+console.log(is instanceof favoriteColor3);
+
+//favoriteFood
+const favoriteFood1 = "Pizza";
+let favoriteFood2 = "Sushi";
+var favoriteFood3 = "Burger";
+console.log(favoriteFood1);
+console.log(favoriteFood2);
+console.log(favoriteFood3);
+console.log(is instanceof favoriteFood1);
+console.log(is instanceof favoriteFood2);
+console.log(is instanceof favoriteFood3);
+
+//hobby
+const hobby1 = "Reading";
+let hobby2 = "Traveling";
+var hobby3 = "Cooking";
+console.log(hobby1);
+console.log(hobby2);
+console.log(hobby3);
+console.log(is instanceof hobby1);
+console.log(is instanceof hobby2);
+console.log(is instanceof hobby3);
+
+//favoriteMovie
+const favoriteMovie1 = "Inception";
+let favoriteMovie2 = "The Matrix";
+var favoriteMovie3 = "The Godfather";
+console.log(favoriteMovie1);
+console.log(favoriteMovie2);
+console.log(favoriteMovie3);
+console.log(is instanceof favoriteMovie1);
+console.log(is instanceof favoriteMovie2);
+console.log(is instanceof favoriteMovie3);
